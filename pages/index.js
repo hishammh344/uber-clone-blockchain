38ba51681d5a25d34a32aa422e115a63384450fd
hishammh <hishammehmood10@gmail.com>
2022-03-09 14:23:52 +0500

    uber-clone-blockchain siurce code

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+const style = {
+  wrapper: `h-screen w-screen flex flex-col`
+}
+
+
+export default function Home() {
+  return (
+    // Header
+    <div className={style.wrapper}>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
